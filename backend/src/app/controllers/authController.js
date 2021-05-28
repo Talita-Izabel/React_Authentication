cd658d7f72d81d684c43c30ee8c09347db5919d3
Talita-Izabel <talitazbl@gmail.com>
2021-05-28 20:56:26 -0300

    auth

--- backend/src/app/controllers/authController.js
+++ backend/src/app/controllers/authController.js
@@ -34,27 +34,6 @@ router.post('/register', async (req, res) => {
   }
 });
 
-/*router.post('/', async(req, res) => {
-  const { email } = req.body;
-
-  try{
-    if( await User.findOne({ email }))
-      return res.status(400).send({ error: 'E-mail já registrado!' });
-
-    const user = await User.create(req.body);
-
-    user.password = undefined;
-
-    return res.send({
-      user,
-      token: generateToken({ id: user.id }),
-    });
-  }
-  catch(err){
-    return res.status(400).send({ error: 'Registration failed'});
-  }
-});*/
-
 router.post('/authenticate', async(req, res) => {
   const { email, password } = req.body;
 
